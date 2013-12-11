2df26a88df96bd2995610ab1fbc2c5a08adf11ed
markseu <mark2011@mayberg.se>
2013-12-11 15:28:23 +0100

    Hello plugins (server include)

---
+++ plugins/include/README.md
@@ -0,0 +1,30 @@
+Include plugin
+==============
+
+Server include for content files.
+
+How do I install this?
+----------------------
+1. Download and install [Yellow](https://github.com/markseu/yellowcms/).  
+2. Download [include.php](include.php?raw=true), copy into your system/plugins folder.  
+
+To uninstall delete the plugin.
+
+How to include information in content files?
+--------------------------------------------
+For a snippet use the format `[include snippet NAME]`, you can add optional arguments.  
+For a text string use the format `[include text NAME]`, you can add an optional language.
+
+All snippets are located in the system/snippets folder, text strings in the system/config folder.
+
+Example
+-------
+Including a snippet, default and optional argument:
+
+    [include snippet breadcrumbs]
+    [include snippet breadcrumbs /]
+
+Including a text string, default and specific language:
+
+    [include text languageDescription]
+    [include text languageDescription sv]
---
+++ plugins/include/include.php
@@ -0,0 +1,43 @@
+<?php
+// Copyright (c) 2013 Datenstrom, http://datenstrom.se
+// This file may be used and distributed under the terms of the public license.
+
+// Server include plugin
+class YellowInclude
+{
+	const Version = "0.1.1";
+	var $yellow;			//access to API
+	
+	// Initialise plugin
+	function onLoad($yellow)
+	{
+		$this->yellow = $yellow;
+	}
+	
+	// Handle custom type parsing
+	function onParseType($name, $text, $typeShortcut)
+	{
+		$output = NULL;
+		if($name=="include" && $typeShortcut)
+		{
+			$args = explode(' ', $text);
+			$type = array_shift($args);
+			switch($type)
+			{
+				case "snippet":	ob_start();
+								call_user_func_array(array($this->yellow, $type), $args);
+								$output = ob_get_contents();
+								ob_end_clean();
+								break;
+				case "text":	list($key, $language) = $args;
+								if(empty($language)) $language = $this->yellow->text->language;
+								$output = htmlspecialchars($this->yellow->text->getText($key, $language));
+								break;
+			}
+		}
+		return $output;
+	}
+}
+
+$yellow->registerPlugin("include", "YellowInclude", YellowInclude::Version);
+?>
