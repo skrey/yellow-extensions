838955dba29e05b022f3b8745b003895974d35d8
markseu <mark2011@mayberg.se>
2014-11-25 08:41:09 +0100

    Hello plugins (web server statistics)

---
+++ plugins/stats/README.md
@@ -0,0 +1,45 @@
+Stats plugin 0.1.1
+==================
+Create statistics from web server logfiles.
+
+How do I install this?
+----------------------
+1. Download and install [Yellow](https://github.com/markseu/yellowcms/).  
+2. Download [stats.php](stats.php?raw=true), copy into your system/plugins folder.  
+
+To uninstall delete the plugin.
+
+How to create statistics?
+-------------------------
+The statistics are available at the command line. It shows referring sites and popular content. Go to your Yellow installation. Type `php yellow.php stats`, you can add optional days and file name. There's a spam filter to remove unwanted sites.
+
+This plugins analyses your web server logfiles, use [Piwik](https://github.com/markseu/yellowcms-extensions/tree/master/snippets/piwik) for more detailed statistics.
+
+Example
+-------
+Creating statistics, different duration and file name:
+
+`php yellow.php stats`  
+`php yellow.php stats 1`  
+`php yellow.php stats 30 /var/log/apache2/website_access.log` 
+
+Here's an example output:
+~~~~
+Referring sites
+
+- 181 http://www.queness.com/post/16142/11-lightning-fast-flat-file-cms
+- 159 http://www.datamation.com/open-source/50-noteworthy-new-open-source-projects-1.html
+- 48 http://upload-magazin.de/blog/9199-keep-it-simple-vom-one-pager-zum-flat-file-cms/
+- 27 http://trendschau.net/blog/uebersicht-flat-file-cms
+- 24 http://www.sunarlim.com/2014/03/flat-file-cms-comparison/
+
+Popular content
+
+- 980 http://datenstrom.se/yellow/
+- 442 http://datenstrom.se/
+- 305 http://datenstrom.se/yellow
+- 177 http://datenstrom.se/ideas/ideas-for-a-new-flat-file-cms-software
+- 140 http://datenstrom.se/contact/
+
+Yellow stats: 30 days, 2903 views
+~~~~
---
+++ plugins/stats/stats.php
@@ -0,0 +1,221 @@
+<?php
+// Copyright (c) 2013-2014 Datenstrom, http://datenstrom.se
+// This file may be used and distributed under the terms of the public license.
+
+// Statistics command plugin
+class YellowStats
+{
+	const Version = "0.1.1";
+	var $yellow;			//access to API
+	var $views;				//detected views
+
+	// Handle plugin initialisation
+	function onLoad($yellow)
+	{
+		$this->yellow = $yellow;
+		$this->yellow->config->setDefault("statsDays", 30);
+		$this->yellow->config->setDefault("statsLinesMax", 10);
+		$this->yellow->config->setDefault("statsLogDir", "/var/log/apache2/");
+		$this->yellow->config->setDefault("statsLogFile", "(.*)access.log");
+		$this->yellow->config->setDefault("statsLocationIgnore", "media|system|edit");
+		$this->yellow->config->setDefault("statsSpamFilter", "website.com");
+	}
+
+	// Handle command help
+	function onCommandHelp()
+	{
+		return "stats [DAYS FILENAME]\n";
+	}
+	
+	// Handle command
+	function onCommand($args)
+	{
+		list($name, $command) = $args;
+		switch($command)
+		{
+			case "stats":	$statusCode = $this->statsCommand($args); break;
+			default:		$statusCode = 0;
+		}
+		return $statusCode;
+	}
+
+	// Create statistics
+	function statsCommand($args)
+	{
+		$statusCode = 0;
+		list($dummy, $command, $days, $fileName) = $args;
+		if($this->checkStaticConfig())
+		{
+			if(empty($days)) $days = $this->yellow->config->get("statsDays");
+			if(empty($fileName))
+			{
+				$path = $this->yellow->config->get("statsLogDir");
+				$regex = "/^".basename($this->yellow->config->get("statsLogFile"))."$/";
+				$fileNames = $this->yellow->toolbox->getDirectoryEntries($path, $regex, true, false);
+				$statusCode = $this->analyseRequests($days, $path, $fileNames);
+			} else {
+				$statusCode = $this->analyseRequests($days, dirname($fileName), array($fileName));
+			}
+		} else {
+			$statusCode = 500;
+			$this->views = 0;
+			$fileName = $this->yellow->config->get("configDir").$this->yellow->config->get("configFile");
+			echo "ERROR checking configuration: Please set serverScheme, serverName and serverBase in file '$fileName'!\n";
+		}
+		echo "Yellow $command: $days day".($days!=1 ? 's' : '').", ";
+		echo "$this->views view".($this->views!=1 ? 's' : '')."\n";
+		return $statusCode;
+	}
+	
+	// Analyse logfile requests
+	function analyseRequests($days, $path, $fileNames)
+	{
+		$this->yellow->toolbox->timerStart($time);
+		$this->views = 0;
+		if(!empty($fileNames))
+		{
+			$statusCode = 200;
+			$content = $sites = $visitors = array();
+			$timeStop = time() - (60 * 60 * 24 * $days);
+			$locationSelf = $this->yellow->config->get("serverBase");
+			$locationIgnore = $this->yellow->config->get("statsLocationIgnore");
+			$spamFilter = $this->yellow->config->get("statsSpamFilter");
+			foreach($fileNames as $fileName)
+			{
+				if(defined("DEBUG") && DEBUG>=1) echo "YellowStats::analyseRequests file:$fileName\n";
+				$fileHandle = @fopen($fileName, "r");
+				if($fileHandle)
+				{
+					$filePos = filesize($fileName)-1; $fileTop = -1;
+					while(($line = $this->getFileLinePrevious($fileHandle, $filePos, $fileTop, $dataBuffer)) !== false)
+					{
+						if(preg_match("/^(\S+) (\S+) (\S+) \[(.+)\] \"(\S+) (.*?) (\S+)\" (\S+) (\S+) \"(.*?)\" \"(.*?)\"$/", $line, $matches))
+						{
+							list($line, $ip, $dummy1, $dummy2, $timestamp, $method, $location, $protocol, $status, $size, $referer, $userAgent) = $matches;
+							if(strtotime($timestamp) < $timeStop) break;
+							$visitorRequestFilter = substru($timestamp, 0, 17).$method.$location;
+							if(preg_match("#$spamFilter#", $line) || $visitors[$ip]==$visitorRequestFilter)
+							{
+								if(defined("DEBUG") && DEBUG>=2) echo "YellowStats::analyseRequests spam:\"$method $location $protocol\" referer:$referer\n";
+								continue;
+							}
+							$visitors[$ip] = $visitorRequestFilter;
+							if(preg_match("#^$locationSelf#", $location))
+							{
+								if(preg_match("#^$locationSelf(.*)/($locationIgnore)/#", $location)) continue;
+								if(preg_match("#^$locationSelf(.*)/robots.txt$#", $location)) continue;
+								if(preg_match("#(bot|spider)#", $userAgent)) continue;
+								if($status >= 400) continue;
+								++$content[$this->getUrl($location)];
+								++$sites[$this->getReferer($referer)];
+								++$this->views;
+							}
+						}
+					}
+					fclose($fileHandle);
+				} else {
+					$statusCode = 500;
+					echo "ERROR reading logfiles: Can't read file '$fileName'!\n";
+				}
+			}
+			if($statusCode == 200)
+			{
+				unset($sites["-"]);
+				$this->showRequests($sites, "Referring sites");
+				$this->showRequests($content, "Popular content");
+			}
+		} else {
+			$statusCode = 500;
+			echo "ERROR reading logfiles: Can't find files in directory '$path'!\n";
+		}
+		$this->yellow->toolbox->timerStop($time);
+		if(defined("DEBUG") && DEBUG>=1) echo "YellowStats::analyseRequests time:$time ms\n";
+		return $statusCode;
+	}
+	
+	// Show top requests
+	function showRequests($array, $text)
+	{
+		if(!empty($array))
+		{
+			echo "$text\n\n";
+			uasort($array, strnatcasecmp);
+			$array = array_reverse($array);
+			$array = array_slice($array, 0, $this->yellow->config->get("statsLinesMax"));
+			foreach($array as $key=>$value) echo "- $value $key\n";
+			echo "\n";
+		}
+	}
+	
+	// Check static configuration
+	function checkStaticConfig()
+	{
+		$serverScheme = $this->yellow->config->get("serverScheme");
+		$serverName = $this->yellow->config->get("serverName");
+		$serverBase = $this->yellow->config->get("serverBase");
+		return !empty($serverScheme) && !empty($serverName) &&
+			$this->yellow->toolbox->isValidLocation($serverBase) && $serverBase!="/";
+	}
+	
+	// Return referer, ignore referers to self
+	function getReferer($referer)
+	{
+		$refererSelf = $this->yellow->config->get("serverName").$this->yellow->config->get("serverBase");
+		return preg_match("#$refererSelf#", $referer) ? "-" : $referer;
+	}
+	
+	// Return URL, with server scheme and server name
+	function getUrl($location)
+	{
+		return $this->yellow->toolbox->getUrl(
+			$this->yellow->config->get("serverScheme"), $this->yellow->config->get("serverName"), "", $location);
+	}
+	
+	// Return previous text line from file, false if not found
+	function getFileLinePrevious($fileHandle, &$filePos, &$fileTop, &$dataBuffer)
+	{
+		if($filePos >= 0)
+		{
+			$line = "";
+			$lineEndingSearch = false;
+			$endPos = $this->getFileLineBuffer($fileHandle, $filePos, $fileTop, $dataBuffer);
+			for(;$filePos>=0; --$filePos)
+			{
+				$currentPos = $filePos - $fileTop;
+				if($dataBuffer[$currentPos]=="\n" && $lineEndingSearch)
+				{
+					$line = substru($dataBuffer, $currentPos+1, $endPos-$currentPos).$line;
+					break;
+				}
+				if($currentPos == 0)
+				{
+					$line = substru($dataBuffer, $currentPos, $endPos-$currentPos+1).$line;
+					$endPos = $this->getFileLineBuffer($fileHandle, $filePos-1, $fileTop, $dataBuffer);
+				}
+				$lineEndingSearch = true;
+			}
+		} else {
+			$line = false;
+		}
+		return $line;
+	}
+	
+	// Update text line buffer
+	function getFileLineBuffer($fileHandle, $filePos, &$fileTop, &$dataBuffer)
+	{
+		if($filePos >= 0)
+		{
+			$top = intval($filePos / 4096) * 4096;
+			if($fileTop != $top)
+			{
+				$fileTop = $top;
+				fseek($fileHandle, $fileTop);
+				$dataBuffer = fread($fileHandle, 4096);
+			}
+		}
+		return $filePos - $fileTop;
+	}
+}
+
+$yellow->plugins->register("stats", "YellowStats", YellowStats::Version);
+?>
