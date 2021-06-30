1b46ea65b14fe4b1c7c0ff6a9534842c713b5314
markseu <mark2011@mayberg.se>
2021-06-30 10:22:21 +0200

    Updated edit and fontawesome

--- source/fontawesome/README-sv.md
+++ source/fontawesome/README-sv.md
@@ -1,6 +1,6 @@
 <p align="right"><a href="README-de.md">Deutsch</a> &nbsp; <a href="README.md">English</a> &nbsp; <a href="README-sv.md">Svenska</a></p>
 
-Fontawesome 0.8.11
+Fontawesome 0.8.12
 ==================
 Ikoner och symboler.
 
--- source/fontawesome/fontawesome.php
+++ source/fontawesome/fontawesome.php
@@ -2,7 +2,7 @@
 // Fontawesome extension, https://github.com/datenstrom/yellow-extensions/tree/master/source/fontawesome
 
 class YellowFontawesome {
-    const VERSION = "0.8.11";
+    const VERSION = "0.8.12";
     public $yellow;         // access to API
     
     // Handle initialisation
