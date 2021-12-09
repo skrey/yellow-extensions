8ec73f38ec2c0c6735d7c165bc6e7c5b3c2999fb
markseu <mark2011@mayberg.se>
2021-12-09 12:03:12 +0100

    Updated tool chain for system testing

--- make-test.php
+++ make-test.php
@@ -2,32 +2,36 @@
 // Datenstrom Yellow extensions, https://github.com/datenstrom/yellow-extensions
 
 if (PHP_SAPI!="cli") {
-    die("ERROR Making test environment: Please run at the command line!\n");
+    die("ERROR making test environment: Please run at the command line!\n");
 } else {
-    $path = "test";
-    if (!is_dir($path)) {
+    if (!is_dir("test")) {
         echo "\rMaking test environment 0%... ";
-        @mkdir("$path/system/extensions", 0777, true);
-        @copy("source/install/yellow.php", "$path/yellow.php");
-        @copy("source/install/robots.txt", "$path/robots.txt");
-        @copy("source/core/core.php", "$path/system/extensions/core.php");
-        @copy("source/update/update.php", "$path/system/extensions/update.php");
+        @mkdir("test/system/extensions", 0777, true);
+        @copy("source/install/robots.txt", "test/robots.txt");
+        @copy("source/install/yellow.php", "test/yellow.php");
+        @copy("source/install/yellow-system.ini", "test/system/extensions/yellow-system.ini");
+        @copy("source/core/core.php", "test/system/extensions/core.php");
+        @copy("source/update/update.php", "test/system/extensions/update.php");
         $directoryHandle = @opendir("zip");
         if ($directoryHandle) {
             while (($entry = readdir($directoryHandle))!==false) {
                 if (substr($entry, 0, 1)==".") continue;
-                @copy("zip/$entry", "$path/system/extensions/$entry");
+                @copy("zip/$entry", "test/system/extensions/$entry");
             }
             closedir($directoryHandle);
         }
-        exec("cd $path; php yellow.php update", $outputLines, $returnStatus);
+        echo "\rMaking test environment 20%... ";
+        $line = date("Y-m-d H:i:s")." info Make test environment for Datenstrom Yellow extensions\n";
+        file_put_contents("test/system/extensions/yellow.log", $line, FILE_APPEND);
+        exec("cd test; php yellow.php update", $outputLines, $returnStatus);
         if ($returnStatus!=0) {
             foreach ($outputLines as $line) echo "$line\n";
             exit($returnStatus);
         }
-        file_put_contents("$path/system/extensions/yellow-system.ini", "CoreStaticUrl: http://website\n", FILE_APPEND);
-        file_put_contents("$path/content/contact/page.md", "exclude\n");   //TODO: remove later, requires better static contact
-        file_put_contents("$path/content/search/page.md", "exclude\n");    //TODO: remove later, requires better static search
+        $settings = "Sitename: Test\nTheme: stockholm\nLanguage: en\nCoreStaticUrl: http://website\n";
+        file_put_contents("test/system/extensions/yellow-system.ini", $settings, FILE_APPEND);
+        file_put_contents("test/content/contact/page.md", "exclude\n");   //TODO: remove later, exclude contact page for now
+        file_put_contents("test/content/search/page.md", "exclude\n");    //TODO: remove later, exclude search page for now
         echo "\rMaking test environment 100%... done\n";
     }
 }
