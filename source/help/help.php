82bd5ad04e4c24e814aabf0d6a66c40fa3cfafe5
markseu <mark2011@mayberg.se>
2022-04-22 18:41:01 +0200

    Updated help, spring cleanup

--- source/help/help.php
+++ source/help/help.php
@@ -2,7 +2,7 @@
 // Help extension, https://github.com/datenstrom/yellow-extensions/tree/master/source/help
 
 class YellowHelp {
-    const VERSION = "0.8.20";
+    const VERSION = "0.8.21";
     public $yellow;         // access to API
 
     // Handle initialisation
